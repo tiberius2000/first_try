64840064be649bdd334cadf1b7771916a843824f
tiberius2000 <33455660+tiberius2000@users.noreply.github.com>
2017-12-12 14:27:42 -0600

    Add files via upload

---
+++ AssignObjectPermission.py
@@ -0,0 +1,136 @@
+import subprocess
+import os
+from subprocess import *
+import sys
+import platform
+
+
+#global variables declarations
+currentDir=''
+pmrepPath=''
+domainFile=''
+
+
+#Function to connect to the repository expects name of repository , domain and user details
+def connect_to_repo(Repository,Domain,User):
+        RepoCommand="pmrep connect -r "+Repository+" -d "+Domain+" -n "+User+" -X DOMAIN_PWD"
+        RepoCommand=RepoCommand.rstrip()
+        p=subprocess.Popen(RepoCommand,stderr=subprocess.PIPE,stdin=subprocess.PIPE,stdout=subprocess.PIPE,shell=True)
+        out,err=p.communicate()
+        if p.returncode or err:
+                print "\n\n ERROR : Connection Failed !!!" #
+                sys.stdout.flush()
+                sys.stdin.flush()
+        else:
+                print "\n\n***********************************************************************\n\nConnection Successful !!!"
+
+                sys.stdout.flush()
+                sys.stdin.flush()
+        return p.returncode
+#END OF FUNCTION connect_to_repo
+
+
+#executes any OS command, here provided pre defined commands
+def execute_pmrep_command(command,output_file_name):
+        out=open(output_file_name,'a')
+        return_code=subprocess.Popen(command,stdin=subprocess.PIPE,stdout=subprocess.PIPE,shell=True)
+        output,error=return_code.communicate()
+        out.writelines(output.strip())
+        out.close()
+        return
+#END OF FUNCTION execute_pmrep_command
+
+#function to make output dirs , create if not exist
+def create_output_directories():
+        if platForm == 'Windows':
+                try:
+                        os.makedirs(currentDir.strip()+"\LogFiles")
+                        print "\n\n***********************************OUTPUT******************************\n\nLog files generated and stored in 'LogFiles' directory" #
+                except OSError:
+                        print "\n\n***********************************OUTPUT******************************\n\nLog files generated and stored in 'LogFiles' directory" #
+                        pass
+        elif platForm == 'Linux':
+                try:
+                        os.makedirs(currentDir.strip()+"/LogFiles")
+                        print "\n\n***********************************OUTPUT******************************\n\nLog files generated and stored in 'LogFiles' directory" #
+                except OSError:
+                        print "\n\n***********************************OUTPUT******************************\n\nLog files generated and stored in 'LogFiles' directory" #
+                        pass
+#END OF FUNCTION create_output_dirs
+
+#checking for all necessary environment variables
+def check_platform():
+        global domainFile
+        global currentDir
+        global pmrepPath
+        global platForm
+        platForm=platform.system()
+        print "Platform recognized : "+platForm
+        if not os.getenv('INFA_HOME'):
+                print "INFA_HOME environment variable is not set in your "+platForm+" platform." #
+                print "\nPlease set INFA_HOME and run the script." #
+                raw_input()
+                sys.exit(0)
+        elif not os.getenv('INFA_DOMAIN_INFO'):
+                print "INFA_DOMAIN_INFO environment variable is not set in your "+platForm+" platform." #
+                print "\nPlease set INFA_DOMAIN_INFO and run the script." #
+                raw_input()
+                sys.exit(0)
+        elif not os.getenv('DOMAIN_PWD'):
+                print "DOMAIN_PWD environment variable not set in your "+platForm+" platform." #
+                print "\nPlease set DOMAIN_PWD and run the script." #
+                raw_input()
+                sys.exit(0)
+        else:
+                if platForm == 'Windows':
+                        pmrepPath=os.getenv('INFA_HOME').strip()+"\clients\PowerCenterClient\client\\bin"
+                elif platForm == 'Linux':
+                        pmrepPath=os.getenv('INFA_HOME').strip()+"/server/bin"
+                currentDir=os.getcwd()
+                domainFile=os.getenv('INFA_DOMAIN_INFO').strip()
+#END OF FUNCTION
+
+                
+#start of the main program body
+if platform.system()=='Windows':
+          os.system('cls')
+elif platform.system()=='Linux':
+          os.system('clear')
+print "Checking for all necessary environment variables ....\n"
+check_platform()
+lines=open(domainFile,'r').readlines()
+os.chdir(pmrepPath)
+if platForm == 'Windows':
+        logFileLoc=currentDir.strip()+"\LogFiles\logPermissions.txt"
+elif platForm == 'Linux':
+        logFileLoc=currentDir.strip()+"/LogFiles/logPermissions.txt"
+for line in lines:
+        fields=line.split(',')
+        Repository=fields[0]
+        Domain=fields[1]
+        User=fields[2]
+        return_code=connect_to_repo(Repository,Domain,User)
+        if return_code:
+                continue
+        permissionFile=raw_input("\n***********************************INPUT*******************************\n\nPlease provide the name of the file, Consists of Permission List: ")
+        if platForm == 'Windows':
+                permissionFile=currentDir.strip()+"\\"+permissionFile.strip()
+        elif platForm == 'Linux':
+                permissionFile=currentDir.strip()+"/"+permissionFile.strip()
+        permission_list_lines=open(permissionFile,'r').readlines()
+        permission_list_lines=permission_list_lines[1:]
+        create_output_directories()
+        for list_permissions in permission_list_lines:
+                field=list_permissions.split(',')
+                ObjType=field[0].strip()
+                ObjName=field[1].strip()
+                GrpName=field[2].strip()
+                seqDomain=field[3].strip()
+                permission=field[4].strip()     
+                command="pmrep assignpermission -o "+ObjType+" -n "+ObjName+" -g "+GrpName+" -s "+seqDomain+" -p "+permission
+                execute_pmrep_command(command,logFileLoc.strip())
+        print "\n\nPermission is assigned to the objects. \n\nFor more details please refer 'logPermission.txt' in LogFiles directory.\n\n***********************************************************************"
+		
+#END PROGRAM
+
+
